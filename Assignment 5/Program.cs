7135c62637a3061725586609d029beb5c091853d
Fares Fouad <faresfouadhussein90@gmail.com>
2026-02-21 22:35:58 +0200

    Add files via upload

--- Assignment 5/Program.cs
+++ Assignment 5/Program.cs
@@ -1,40 +1,30 @@
 ﻿namespace Assignment_5
 {
-    class MyCookieCollection
+    class Grades
     {
+        public int[] grades;
+        public Grades()
+        {
+            grades = new int[5];
+        }
+        public int this[int index]
+        {
+            get { return grades[index]; }
+            set { grades[index] = value; }
+        }
 
-        Dictionary<string, string> cookies;
-        public MyCookieCollection()
-        {
-            cookies = new Dictionary<string, string>();
-        }
-        public string this[string key]
-        {
-            get
-            {
-                if (cookies.ContainsKey(key))
-                {
-                    return cookies[key];
-                }
-                return null;
-            }
-            set
-            {
-                cookies[key] = value;
-            }
-        }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            MyCookieCollection cookies = new MyCookieCollection();
-            cookies["username"] = "John Doe";
-            cookies["theme"] = "dark";
-            cookies["language"] = "en";
-            Console.WriteLine("Username: " + cookies["username"]);
-            Console.WriteLine("Theme: " + cookies["theme"]);
-            Console.WriteLine("Language: " + cookies["language"]);
+            Grades grades = new Grades();
+            grades[0] = 95;
+            grades[1] = 88;
+            Console.WriteLine(grades[0]);
+            Console.WriteLine(grades[1]);
+
+
 
         }
     }
